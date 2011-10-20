9a813105a3f0702768602c3334b2bce950c1d24a
Jianjun Wu <jxjstony@gmail.com>
2011-10-20 22:44:57 +0800

    update UIDevice-Hardware for iphone 4s

--- UIDevice-Hardware.h
+++ UIDevice-Hardware.h
@@ -12,6 +12,7 @@
 #define IPHONE_3G_NAMESTRING            @"iPhone 3G"
 #define IPHONE_3GS_NAMESTRING           @"iPhone 3GS" 
 #define IPHONE_4_NAMESTRING             @"iPhone 4" 
+#define IPHONE_4S_NAMESTRING             @"iPhone 4S"
 #define IPHONE_5_NAMESTRING             @"iPhone 5"
 #define IPHONE_UNKNOWN_NAMESTRING       @"Unknown iPhone"
 
@@ -46,6 +47,7 @@ typedef enum {
     UIDevice3GiPhone,
     UIDevice3GSiPhone,
     UIDevice4iPhone,
+    UIDevice4SiPhone,
     UIDevice5iPhone,
     
     UIDevice1GiPod,
